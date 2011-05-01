d3fc76e9f9a0c7bb5257ebb5ecfb8289363634d1
Daniel Hernández <daniel@degu.cl>
2011-05-01 15:22:14 -0300

    Files in theme
    We add files in theme to the static resource.

--- lib/queule.rb
+++ lib/queule.rb
@@ -16,13 +16,26 @@ module Queule
   # 
   # @param options [Hash]
   def self.setup(options = {})
-    uri_prefix = options[:uri_prefix] || '/queule/'
-    theme_path = options[:theme_path] || 'queule/theme'
-    # Register script
     dir = File.dirname(File.expand_path(__FILE__))
+    uri_prefix = options[:uri_prefix] || '/queule'
+    theme_path = options[:theme_path] || File.join(dir, 'theme')
     script_file = File.join(dir, 'queule.js')
     Ruil::StaticResource.new do |resource|
-      resource.add(script_file, uri_prefix + 'script.js', 'application/javascript')
+      resource.add(script_file, uri_prefix + '/script.js', 'application/javascript')
+      Dir[File.join(theme_path,'*')].each do |file_name|
+        content_type = case file_name
+                       when /\.css/
+                         'text/css'
+                       when /\.png/
+                         'image/png'
+                       else
+                         nil
+                       end
+        unless content_type.nil?
+          file_uri = file_name.sub(/^#{theme_path}/, uri_prefix + '/theme')
+          resource.add(file_name, file_uri, content_type)
+        end
+      end
     end
   end
 end
--- spec/queule_spec.rb
+++ spec/queule_spec.rb
@@ -15,4 +15,21 @@ describe Queule do
     response.content_type.should == 'application/javascript'
   end
 
+  it 'should get the theme files' do
+    Dir['lib/theme/*'].each do |file_name|
+      request = {
+        'REQUEST_METHOD' => 'GET',
+        'PATH_INFO'      => file_name.sub(/lib/, '/queule')
+      }
+      response = Ruil::Register.call(request)
+      response.status.should == 200
+      case file_name
+      when /\.css/
+        response.content_type.should == 'text/css'
+      when /\.png/
+        response.content_type.should == 'image/png'
+      end
+    end
+  end
+
 end
